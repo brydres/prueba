a37475cc7ce45bb36b1e9e360fe54e55ef452872
brydres <bryanb1489@gmail.com>
2023-07-17 13:54:05 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import DotsCard from './components/DotsCard'
+
+function App() {
+
+  return (
+   <DotsCard/>
+  )
+}
+
+export default App
---
+++ src/components/DotsCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function DotsCard() {
+  return (
+    <>
+    <div className='card'>
+        <div className='card__img'>
+            <img src="htpps://1000logos.net/wp-content/uploads/2021/05/GitHub-logo.png" />
+            <div className='card__grideffect'>
+                {Array.from ({ length: 100}, (_, index) => (
+                    <a key={index} className='card__grid-effect-tile' href="#"></a>
+                ))}
+
+            </div>
+        </div>
+
+    </div>
+    
+    </>
+  )
+}
